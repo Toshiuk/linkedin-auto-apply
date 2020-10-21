1d8601b14658deddc0c905c39df964405305709b
ext_ssubramani <ext_ssubramani@falabella.cl>
2020-10-21 22:31:16 +0530

    Update to Puppeteer from Selenium

--- index.js
+++ index.js
@@ -1,118 +1,37 @@
-import { Builder, By, Key, until, promise } from 'selenium-webdriver';
-const SECOND = 1000;
-const driver = new Builder().withCapabilities({ browserName: 'chrome', chromeOptions: { w3c: false } }).build();
-const wait = (delay, ...args) => new Promise(resolve => setTimeout(resolve, delay, ...args));
-
+const puppeteer = require("puppeteer");
 const username = "email@email.com";
 const password = "senha";
 
-const searchTerm = "react".replace( " ", "%20" );
-const searchArea = "inglaterra".replace( " ", "%20" );
+const searchTerm = "react".replace(" ", "%20");
+const searchArea = "inglaterra".replace(" ", "%20");
 
 let totalApplied = 0;
 
-const insertFieldById = async ( id, value ) => {
-    await driver.wait( until.elementsLocated( By.id( id ) ), 20 * SECOND );
-
-    const field = driver.findElement( By.id( id ) );
-
-    await field.clear();
-    await wait( 1 * SECOND );
-    await field.sendKeys( value );
+const insertFieldById = async (id, value, page) => {
+    await page.waitForSelector('input[id=' + id + ']');
+    await page.type('input[id=' + id + ']', value);
+    await page.click('[class="btn__primary--large from__button--floating mercado-button--primary"]');
 }
 
-const hideById = ( id ) => {
-    driver.executeScript(`document.querySelector('#${id}').setAttribute('style', 'display: none')`);
+const loginPage = async (page) => {
+    await insertFieldById("username", username, page);
+    await insertFieldById("password", password, page);
 }
 
-const loginPage = async () => {
-    await insertFieldById( "username", username );
-
-    await insertFieldById( "password", password );
-
-    const passwordField = driver.findElement( By.id( "password" ) );
-
-    await wait( 2 * SECOND );
-    await passwordField.submit();
+const jobsList = async (page) => {
+//check for joblist
 }
-
-const jobsList = async () => {
-    await driver.wait( until.elementsLocated( By.className( "artdeco-pagination__pages" ) ), 20 * SECOND );
-    let pages = new Array(1);
-    const pagesList = await driver.findElements( By.className( "artdeco-pagination__pages" ) );
-    if( pagesList.length !== 0 ){ 
-         pages = await pagesList[0].findElements( By.tagName( "li" ) );
-    } 
-    
-    for( let j = 1; j <= pages.length ; j++ ){
-        await wait( 2 * SECOND );
-        hideById( "msg-overlay" );
-        await driver.wait( until.elementsLocated( By.className( "jobs-search-results__list" ) ), 20 * SECOND );
-        const jobs = await driver.findElement( By.className( "jobs-search-results__list" ) ).findElements( By.tagName( "li" ) );
-        for( let i = 0; i < jobs.length ; i++ ){
-            await wait( 1 * SECOND );
-            const job = await jobs[i].findElements( By.className( "job-card-search__title" ) );
-            if( job.length !== 0 ){
-                job[0].click();
-                await wait( 2 * SECOND );
-                const alreadyApplied = await driver.findElements( By.className( "jobs-s-apply__applied-date" ) );
-                if( alreadyApplied.length === 0 ){
-                    console.log("Aplicando vaga...")
-                    await wait( 2 * SECOND );
-                    await driver.wait( until.elementsLocated( By.className( "jobs-apply-button" ) ), 20 * SECOND );
-                    await driver.findElement( By.className( "jobs-apply-button" ) ).click();
-                    await wait( 1 * SECOND );
-                    await driver.wait( until.elementsLocated( By.className( "jobs-easy-apply-footer__actions" ) ), 20 * SECOND );
-                    const progressBar = await driver.findElements( By.className( "jobs-easy-apply-content__progress-bar" ) );
-                    if( progressBar.length === 0 ){ 
-                        const applyButton = await driver.findElement( By.className( "jobs-easy-apply-footer__actions" ) ).findElement( By.tagName( "button" ) );
-                        applyButton.click();
-                        job[0].findElement( By.tagName( "a" ) ).getText().then( jobName => console.log( `Vaga aplicada para ${jobName}` ) );
-                        await wait( 2 * SECOND );
-                        totalApplied++;
-                    } else {
-                        console.log( "Pulando vaga, existem muitas etapas para poder aplicar" );
-                        driver.findElement( By.className( "artdeco-modal__dismiss" ) ).click();
-                        await driver.wait( until.elementsLocated( By.className( "artdeco-modal__actionbar" ) ), 20 * SECOND );
-                        const cancelButton = await driver.findElement( By.className( "artdeco-modal__actionbar" ) ).findElements( By.tagName( "button" ) );
-                        cancelButton[1].click();
-                        await wait( 1 * SECOND );
-                    }
-                } else {
-                    console.log("Vaga já aplicada!");
-                }
-            } 
-        } 
-
-        if ( j < pages.length ) {
-            await wait( 1 * SECOND );
-            console.log("Indo para proxima página!");
-            pages[j].click();
-            await wait( 2 * SECOND );
-        }
-
-    }
-
-      
-}
-
 
 const start = async () => {
-    driver.get("https://www.linkedin.com/");
-    driver.findElement( By.className( "nav__button-secondary" ) ).click();
-
-    await loginPage();
-
-    await wait( 3 * SECOND );
-    await driver.get(`https://www.linkedin.com/jobs/search/?f_LF=f_AL&keywords=${searchTerm}&location=${searchArea}&sortBy=DD`);
-
-    await wait( 2 * SECOND );
-
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://www.linkedin.com/");
+    await page.click('[class="nav__button-secondary"]');
+    await loginPage(page);
+    await page.goto(`https://www.linkedin.com/jobs/search/?f_LF=f_AL&keywords=${searchTerm}&location=${searchArea}&sortBy=DD`);
     await jobsList();
-
     console.log(`Finalizado. Aplicado para ${totalApplied} vaga(s)!`)
-   
+    await browser.close();
 }
-
 
 start();
